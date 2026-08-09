f4f68f5efbc849d57027aa724874c896e53d9ddb
qiaojingbo <3152912378@qq.com>
2026-08-09 21:14:58 +0800

    refactor(application): 收拢应用生命周期

--- src/windcode/application/__init__.py
+++ src/windcode/application/__init__.py
@@ -4,10 +4,12 @@ from windcode.application.extensions import (
     ExtensionRunLease,
     McpStartupStatus,
 )
+from windcode.application.lifecycle import ApplicationLifecycle
 from windcode.application.providers import ProviderApplication
 from windcode.application.runs import RunApplication
 
 __all__ = [
+    "ApplicationLifecycle",
     "ConfigurationApplication",
     "ExtensionApplication",
     "ExtensionRunLease",
